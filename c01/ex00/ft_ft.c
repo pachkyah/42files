b9f807789fb66942108ec9cf2951e5ee35fb8ee2
pachkyah <yahor.pachkouski@gmail.com>
2025-04-12 18:40:20 +0200

    ex00 in c01

---
+++ c01/ex00/ft_ft.c
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_ft.c                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ypachkou <ypachkou@student.42prague.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/04/12 18:25:39 by ypachkou          #+#    #+#             */
+/*   Updated: 2025/04/12 18:32:32 by ypachkou         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+//#include <stdio.h>
+
+void ft_ft(int *nbr)
+/*
+    Takes in adress of int, changes whatever is there to 42
+*/
+{
+    *nbr = 42;
+}
+
+/*
+int main(void)
+{
+    int a = 13;
+    ft_ft(&a);
+    printf("a: %d\n", a);
+    return 0;
+}
+*/
